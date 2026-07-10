f89217bbae44eef99bb370bf4fbc7c35c724f216
abbysheksharma <abbysheksharma@gmail.com>
2026-07-10 09:16:04 +0530

    day 33-34

---
+++ Day 6 - Day 38/Module-5-Numpy/03-shape/shape.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+arr = np.array([10, 20, 30, 40])
+
+arr1 = np.array([
+    [10, 20, 30],
+    [40, 50, 60]
+])
+
+arr2 = np.array([
+    [
+        [1, 2],
+        [3, 4]
+    ],
+    [
+        [5, 6],
+        [7, 8]
+    ]
+])
+
+print(arr2.shape)
